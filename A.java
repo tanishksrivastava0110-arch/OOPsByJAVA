fb38e0b8c562fb3035df373ff6ef6a7e45b83482
tanishksrivastava0110-arch <tanishksrivastava0110@gmail.com>
2026-09-11 12:19:30 +0530

    Rental

--- A.java
+++ A.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 public class A{
     int a,b,c;
     A(){
@@ -23,29 +22,3 @@ public class A{
         
     }
 }
-=======
-public class A{
-    int a,b,c;
-    A(){
-        System.out.println("Default");
-    }
-    A(int a){
-        this.a=a;
-    }
-    A(int a,int b){
-        this.a=a;
-        this.b=b;
-    }
-    A(int a,int b,int c){
-        this.a=a;
-        this.b=b;
-        this.c=c;
-    }
-    public static void main(String[] args) {
-        A obj1=new A();
-        A obj2=new A(10);
-        System.out.println(obj1.a+" "+obj2.b+" "+ obj2.c);
-        
-    }
-}
->>>>>>> b0b0f58f1fbfc65e0a0dad5d5d236e289822cefc
--- helloworld.java
+++ helloworld.java
@@ -1,12 +1,5 @@
-<<<<<<< HEAD
 class helloworld{
     public static void main(String[] args) {
         System.out.println("Hello world.");
     }
-=======
-class helloworld{
-    public static void main(String[] args) {
-        System.out.println("Hello world.");
-    }
->>>>>>> b0b0f58f1fbfc65e0a0dad5d5d236e289822cefc
 }
